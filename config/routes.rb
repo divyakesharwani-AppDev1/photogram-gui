c207f351bfb2724a2a719c287b9f4e539a55f4b1
divyakesharwani <dkesharw@chicagobooth.edu>
2022-02-20 08:54:06 +0000

    SatNightwork

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
+
+  get("/photos", { :controller => "photos", :action => "index" })
+
+  get("/users/:a_user", { :controller => "users", :action => "showdetail" })
 
 end
