773c0810c5833822ac411341838f4f904ea590bd
divyakesharwani <dkesharw@chicagobooth.edu>
2022-02-21 09:01:40 +0000

    photo index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,11 @@
 class PhotosController < ApplicationController
 
   def index
-  
+
+
+    @allphotos = Photo.all.order({:created_at => :desc})
     render({:template => "/photo_template/index.html.erb"})
   end
 
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-
-    @alluser = User.all.order({:username=> :asc})
+     @alluser = User.all.order({:username=> :asc})
 
     render({:template => "/user_template/index.html.erb"})
   end
 
   def showdetail
     #Parameters: {"a_user"=>":a_user"}
+    
 
     @theuser = params.fetch("a_user")
 
     @theusername = User.all.where({:username => @theuser}).at(0)
 
-
-
-    render(:template=>"user_template/show.html.erb")
+    if @theusername == nil
+      redirect_to("/404")
+    else
+     render(:template=>"user_template/show.html.erb")
+    end
   end
 
 
